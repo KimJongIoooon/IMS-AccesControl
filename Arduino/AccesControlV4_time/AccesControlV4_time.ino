30f9524ef6bfc76aec3c455d1af3d67aef2b6a20
Iain den Houting <Iain1998@live.nl>
2019-11-04 16:39:42 +0100

    added functionality to add id's from serial command

--- Arduino/AccesControlV4_time/AccesControlV4_time.ino
+++ Arduino/AccesControlV4_time/AccesControlV4_time.ino
@@ -211,9 +211,9 @@ void setup() {
 
 ///////////////////////////////////////// Main Loop ///////////////////////////////////
 void loop () {
-   do {
+  do {
     serialCommunicationIn();
-  
+
     successRead = getID();  // sets successRead to 1 when we get read from reader otherwise 0
     // When device is in use if wipe button pressed for 10 seconds initialize Master Card wiping
     if (digitalRead(wipeB) == LOW) { // Check if button is pressed
@@ -258,7 +258,7 @@ void loop () {
       }
       else {                    // If scanned card is not known add it
         Serial.println(F("I do not know this PICC, adding..."));
-        writeID(readCard);
+        //writeID(readCard);
         Serial.println(F("-----------------------------"));
         Serial.println(F("Scan a PICC to ADD or REMOVE to EEPROM"));
       }
@@ -283,20 +283,20 @@ void loop () {
         Serial.print ("hours:");
         Serial.print("start Time:");
         for (uint8_t i = 0; i < 2; i++) {  //
-          
+
           Serial.print(storedHours[i], DEC);
         }
         Serial.print("End Time:");
-        
+
         int startHour = storedHours[0];
-        int startMinutes= storedHours[1];; 
-        int endHour= storedHours[2];;
-        int endMinutes= storedHours[3];; 
-s        if(startHour <  hour(t) && startMinutes <  minute(t)){
-          Serial.print("Kom binnen");
-        } else {
-          
-        }
+        int startMinutes = storedHours[1];
+        int endHour = storedHours[2];
+        int endMinutes = storedHours[3];
+        //        if(startHour <  hour(t) && startMinutes <  minute(t)){
+        //          Serial.print("Kom binnen");
+        //        } else {
+        //          Serial.print("Kom binnen");
+        //        }
         for (uint8_t i = 2; i < 4; i++) {  //
           Serial.print(storedHours[i], DEC);
         }
@@ -411,7 +411,7 @@ void readID( uint8_t number ) {
 }
 
 ///////////////////////////////////////// Add ID to EEPROM   ///////////////////////////////////
-void writeID( byte a[] ) {
+void writeID( byte a[],  byte hours[]) {
   if ( !findID( a ) ) {     // Before we write to the EEPROM, check to see if we have seen this card before!
     uint8_t num = EEPROM.read(0);     // Get the numer of used spaces, position 0 stores the number of ID cards
     uint8_t start = ( num * 8 ) + 10;  // Figure out where the next slot starts
@@ -419,10 +419,10 @@ void writeID( byte a[] ) {
     EEPROM.write( 0, num );     // Write the new count to the counter
     for ( uint8_t j = 0; j < 4; j++ ) {   // Loop 4 times
       EEPROM.write( start + j, a[j] );  // Write the array values to EEPROM in the right position
-      EEPROM.write( start + j + 4, hours[j]); //Wrote hpirs 
+      EEPROM.write( start + j + 4, hours[j]); //Wrote hpirs
     }
-    
-    
+
+
     successWrite();
     Serial.println(F("Succesfully added ID record to EEPROM"));
   }
@@ -486,7 +486,7 @@ uint8_t findIDSLOT( byte find[] ) {
 ///////////////////////////////////////// Find ID From EEPROM   ///////////////////////////////////
 bool findID( byte find[] ) {
   uint8_t count = EEPROM.read(0);     // Read the first Byte of EEPROM that
-  for ( uint8_t i = 1; i < count; i++ ) {    // Loop once for each EEPROM entry
+  for ( uint8_t i = 0; i < count; i++ ) {    // Loop once for each EEPROM entry
     readID(i);          // Read an ID from EEPROM, it is stored in storedCard[4]
     if ( checkTwo( find, storedCard ) ) {   // Check to see if the storedCard read from EEPROM
       return true;
@@ -590,9 +590,90 @@ void serialCommunicationIn() {
   }
 }
 
-void executeSerialCommand(String message){
-  Serial.print(message);
-  Serial.println("execute command");
+void executeSerialCommand(String command) {
+  Serial.print("executing command: ");
+  Serial.println(command);
+  if (command.startsWith("ADD_CARD:")) {
+    int indexValueStart = command.indexOf(':') + 1;
+    int indexValueEnd = command.length();
+    String value = command.substring(indexValueStart, indexValueEnd);
+
+    //Convert to char array for function
+    char charArray[8];
+    value.toCharArray(charArray, 255);
+    byte id[4] = {0};
+    hexCharacterStringToBytes(id, charArray);
+    //addCard(id);
+  }
+}
+
+void addCard(byte id[], byte hours[]) {
+  Serial.print("Adding ID:"); 
+  for ( uint8_t i = 0; i < 4; i++) {  //
+    Serial.print(id[i], HEX);
+  }
+  Serial.println(".");
+    writeID(id, hours);
+
+}
+void hexCharacterStringToBytes(byte *byteArray, const char *hexString)
+{
+  bool oddLength = strlen(hexString) & 1;
+
+  byte currentByte = 0;
+  byte byteIndex = 0;
+
+  for (byte charIndex = 0; charIndex < strlen(hexString); charIndex++)
+  {
+    bool oddCharIndex = charIndex & 1;
+
+    if (oddLength)
+    {
+      // If the length is odd
+      if (oddCharIndex)
+      {
+        // odd characters go in high nibble
+        currentByte = nibble(hexString[charIndex]) << 4;
+      }
+      else
+      {
+        // Even characters go into low nibble
+        currentByte |= nibble(hexString[charIndex]);
+        byteArray[byteIndex++] = currentByte;
+        currentByte = 0;
+      }
+    }
+    else
+    {
+      // If the length is even
+      if (!oddCharIndex)
+      {
+        // Odd characters go into the high nibble
+        currentByte = nibble(hexString[charIndex]) << 4;
+      }
+      else
+      {
+        // Odd characters go into low nibble
+        currentByte |= nibble(hexString[charIndex]);
+        byteArray[byteIndex++] = currentByte;
+        currentByte = 0;
+      }
+    }
+  }
+}
+
+byte nibble(char c)
+{
+  if (c >= '0' && c <= '9')
+    return c - '0';
+
+  if (c >= 'a' && c <= 'f')
+    return c - 'a' + 10;
+
+  if (c >= 'A' && c <= 'F')
+    return c - 'A' + 10;
+
+  return 0;  // Not a valid hexadecimal character
 }
 bool monitorWipeButton(uint32_t interval) {
   uint32_t now = (uint32_t)millis();
